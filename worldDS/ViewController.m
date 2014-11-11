541cd6cf41d8b73693980ba6ef7640525943b1f8
sho634 <sho634529@gmail.com>
2014-11-11 20:57:36 +0800

    長押しでピンを落とす事ができるようになりました。スタバまで来て頂きありがとうございました。

--- worldDS/ViewController.m
+++ worldDS/ViewController.m
@@ -18,63 +18,53 @@ MKMapView* _mapView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-/////////////////////////////マップ/////////////////////////////////////////////////////////
-    
-    //MapViewオブジェクト
-    MKMapView *mapView = [[MKMapView alloc] init];
-    mapView.delegate = self;
-    //大きさ,位置決定
-    mapView.frame = CGRectMake(0, 20, 320, 460);
-    //表示位置を設定
+//mmmmmmmmmmmmmmmmmmmmmmmmmmm地図の表示mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+    _mapView = [[MKMapView alloc] init];
+    _mapView.delegate = self;
+    _mapView.frame = CGRectMake(0, 20, 320, 460);
     CLLocationCoordinate2D co;
-    //日本（東京）の位置を設定する
     co.latitude = 35.689488;       //緯度
     co.longitude = 139.691706;     //軽度
-    //縮尺するためにViewに追加
-    MKCoordinateRegion cr = mapView.region;
-    
+    MKCoordinateRegion cr = _mapView.region;
     cr.span.latitudeDelta = 0.05;
-    //小さくすると詳細な地図になる
     cr.span.longitudeDelta = 0.05;
-    
     cr.center = co;
-    
-    [mapView setRegion:cr];
+    [_mapView setRegion:cr];
     
     //地図の表示種類設定
-    mapView.mapType = MKMapTypeStandard;
+    _mapView.mapType = MKMapTypeStandard;
     
     //現在地を表示
-    mapView.showsUserLocation = YES;
+    _mapView.showsUserLocation = YES;
     
-[self.view addSubview:mapView];
+[self.view addSubview:_mapView];
+//
+    _mapView.delegate = self;
 
-
-//mmmmmmmmmmmmmmmmmmmmmmmmロングジェスチャーの設定とアクション設定mmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-UILongPressGestureRecognizer *longPressGesture;
-longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self
-                                                                 action:@selector(handleLongPressGesture:)];
+//mmmmmmmmmmmmmmmmmmmmmmmmロングジェスチャーの設定とアクション設定mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+  
+    
+    UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPressGesture:)];
 [_mapView addGestureRecognizer:longPressGesture];
 _mapView.delegate = self;
-    NSLog(@"longps");
+   
     
-    
-    
-    
-    
-
-
-}
-
+    }
+//mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+   
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 //mmmmmmmmmmmmmmmmmmmmmmロングジェスチャーのメソッドmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-    - (void)handleLongPressGesture:(UILongPressGestureRecognizer *)gesture {
-        NSLog(@"handleLongPressGesture");
-        if (gesture.state == UIGestureRecognizerStateBegan) { //長押し検出時のみ作動
-            
+- (void)handleLongPressGesture:(UILongPressGestureRecognizer *)gesture {
+    
+    //長押し検出時のみ作動
+    if ([gesture state] == UIGestureRecognizerStateBegan){
+                NSLog(@"handleLongPressGesture");
+        
+            }else if([gesture state] == UIGestureRecognizerStateEnded){
             CGPoint touchedPoint = [gesture locationInView:_mapView];
             
             NSLog(@"touchedPoint x:[%f]", touchedPoint.x);
@@ -85,7 +75,7 @@ _mapView.delegate = self;
             NSLog(@"touchCoordinate latitude:%f  longitude:%f", touchCoordinate.latitude, touchCoordinate.longitude);
             
             
-            //[self setAnnotation:touchCoordinate mapMove:NO animated:NO];
+            [self setAnnotation:touchCoordinate mapMove:NO animated:NO];
             
             CLLocation *location = [[CLLocation alloc] initWithLatitude:touchCoordinate.latitude longitude:touchCoordinate.longitude];
             float fLat = location.coordinate.latitude;
@@ -96,9 +86,57 @@ _mapView.delegate = self;
             NSLog(@"location = %@",location);
             //[_allPinArray2 addObject:location];
             //[_allPinArray addObject:str];
-        }
-
-
 }
+}
+-(void)setAnnotation:(CLLocationCoordinate2D) point mapMove:(BOOL)mapMove animated:(BOOL)animated {
+    // ピンを全て削除
+    //    [_mapView removeAnnotations: _mapView.annotations];
+    // 新しいピンを作成
+    MKPointAnnotation *anno = [[MKPointAnnotation alloc] init];
+    anno.coordinate = point;
+    int n = 0;
+    anno.title = [NSString stringWithFormat:@"PIN-%d",n];
+    ++n;
+    anno.subtitle = [NSString stringWithFormat:@"緯度:%f 経度:%f",
+                     anno.coordinate.latitude,anno.coordinate.longitude];
+    
+    // マップの表示を変更
+    if (mapMove) {
+        MKCoordinateSpan CoordinateSpan = MKCoordinateSpanMake(0.005,0.005);
+        MKCoordinateRegion CoordinateRegion = MKCoordinateRegionMake(point,CoordinateSpan);
+        [_mapView setRegion:CoordinateRegion animated:animated];
+    }
+    // ピンを追加
+    [_mapView addAnnotation:anno];
+    
+    // ピンの周りに円を表示
+    //    MKCircle* circle = [MKCircle circleWithCenterCoordinate:point radius:500];  // 半径500m
+    //    [_mapView removeOverlays:_mapView.overlays];
+    //    [_mapView addOverlay:circle];
+}
+
+//mmmmmmmmmmmmmmmmmmmmmmmmピン落ちてくるアニメーションのメソッドmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
+    {
+        static NSString *pinIndentifier = @"PinAnnotationID";
+        
+        //ピン情報の再利用
+        MKPinAnnotationView *pinView = (MKPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:pinIndentifier];
+        
+        if (pinView == nil){
+            pinView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:pinIndentifier];
+            pinView.animatesDrop = YES;
+            pinView.canShowCallout = YES;
+            
+            pinView.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+        }
+        
+        return pinView;
+    }
+
+
 
 @end
